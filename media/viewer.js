8c2a2979cab43617fecebadc1cf53a72324fdb3a
David Sun <natoally@outlook.com>
2023-11-15 16:13:07 -0800

    communication between webview and vscode working

--- media/viewer.js
+++ media/viewer.js
@@ -1,50 +1,37 @@
 // this script will be run within the webview itself,
 // and cannot access the main vscode APIs directly.
+
+// get this once only and save it for later use
+const vscode = acquireVsCodeApi();
+
+function handleUpdate(message) {
+    console.log('handle message');
+    console.log(message);
+    vscode.postMessage({ command: 'warn', text: 'lkasdfjklasjkldflkjasdf' });
+}
+
+function loadImage(buffer) {
+    vscode.postMessage({ command: 'info', text: `data size in bytes: ${buffer.byteLength}` });
+    const container = document.getElementById('preview-container');
+    const canvas = document.getElementById('preview-canvas');
+}
+
 (function () {
-    const message = JSON.parse(jsonStr);
-    const { data, width, height } = message;
-
-    scaleCanvas(1.0);
-
-    function scaleCanvas(scale) {
-        // write the pixels to an ImageData object
-        const pixels = new Uint8ClampedArray(width * height * 4);
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                const i = (y * width + x) * 4;
-                //let color = data[y * width + x];
-                pixels[i + 0] = x;//color.r;
-                pixels[i + 1] = y;//color.g;
-                pixels[i + 2] = 0;//color.b;
-                pixels[i + 3] = 255;
-            }
-        }
-        const img = new ImageData(pixels, width, height);
-
-        // write the ImageData to a background canvas
-        const backCanvas = document.createElement('canvas');
-        backCanvas.width = img.width;
-        backCanvas.height = img.height;
-        backCanvas.getContext('2d').putImageData(img, 0, 0);
-
-        // scale the target canvas and write the background canvas to it
-        const targetCanvas = document.getElementById('canvas-area');
-        targetCanvas.width = img.width * scale;
-        targetCanvas.height = img.height * scale;
-        const ctx = targetCanvas.getContext('2d');
-        ctx.scale(scale, scale);
-        ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(backCanvas, 0, 0);
-    }
-
     // handle messages sent from the extension to the webview
     window.addEventListener('message', event => {
         const message = event.data;
-        switch (message.type) {
+        switch (message.command) {
             case 'update':
-                {
-                    break;
-                }
+                handleUpdate(message);
+                break;
+            case 'load':
+                // convert raw byte array to typed data array for loading into data view
+                const buffer = message.image;
+                loadImage(buffer);
+                break;
         }
     });
+
+    // inform vscode we are ready to receive messages
+    vscode.postMessage({ command: 'ready' });
 }());
--- src/viewer.ts
+++ src/viewer.ts
@@ -5,6 +5,8 @@ class ImagePreviewDocument extends vscode.Disposable implements vscode.CustomDoc
 
     public readonly uri: vscode.Uri;
 
+    private _panel: vscode.WebviewPanel | undefined;
+
     public static async create(uri: vscode.Uri): Promise<ImagePreviewDocument> {
         return new ImagePreviewDocument(uri);
     }
@@ -12,6 +14,80 @@ class ImagePreviewDocument extends vscode.Disposable implements vscode.CustomDoc
     private constructor(uri: vscode.Uri) {
         super(() => { });
         this.uri = uri;
+    }
+
+    public dispose(): void {
+    }
+
+    public async initPanel(panel: vscode.WebviewPanel, context: vscode.ExtensionContext): Promise<void> {
+        this._panel = panel;
+
+        const storageUri = context.globalStorageUri;
+        const previewUri = vscode.Uri.joinPath(storageUri, 'preview.png');
+
+        // convert local path of project files to a uri we can use in the webview
+        const previewSrc = panel.webview.asWebviewUri(previewUri);
+
+        // convert texture to png and write to disk so webview can read from disk
+        const previewData = await PVRLoader.readFile(this.uri);
+        await vscode.workspace.fs.createDirectory(storageUri);
+        await vscode.workspace.fs.writeFile(previewUri, previewData);
+
+        // use a content security policy to only allow loading styles from our extension directory
+        const mediaUri = vscode.Uri.joinPath(context.extensionUri, 'media');
+        const styleSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'viewer.css'));
+        const scriptSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'viewer.js'));
+ 
+        // use a nonce to only allow a specific script to be run
+        const nonce = getNonce();
+
+        // setup initial content in new webview
+        panel.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [mediaUri, storageUri]
+        };
+        panel.webview.html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src *; style-src ${panel.webview.cspSource}; script-src 'nonce-${nonce}';">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="${styleSrc}" rel="stylesheet">
+</head>
+<body>
+    <div id="preview-container" data-vscode-context='{"preventDefaultContextMenuItems":true}'>
+        <canvas id="preview-canvas"></canvas>
+        <img class="preview" draggable="false" src="${previewSrc}">
+    </div>
+    <script nonce="${nonce}" src="${scriptSrc}"></script>
+</body>
+</html>`;
+
+        // handle messages from webview
+        panel.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'ready':
+                    this.onWebviewReady();
+                    break;
+                case 'info':
+                    vscode.window.showInformationMessage(message.text);
+                    break;
+                case 'warn':
+                    vscode.window.showWarningMessage(message.text);
+                    break;
+                case 'error':
+                    vscode.window.showErrorMessage(message.text);
+                    break;
+            }
+        }, null, context.subscriptions);
+    }
+
+    public onWebviewReady() {
+        // post initial message for webview to process
+        const buffer = new ArrayBuffer(400);
+        const view = new Uint32Array(buffer);
+        for (let i = 0; i < 100; i++) { view[i] = i; }
+        this._panel?.webview.postMessage({ command: 'load', image: buffer });
     }
 }
 
@@ -39,44 +115,7 @@ export default class ImagePreviewProvider implements vscode.CustomReadonlyEditor
     }
 
     public async resolveCustomEditor(document: ImagePreviewDocument, panel: vscode.WebviewPanel, _token: vscode.CancellationToken): Promise<void> {
-        const storageUri = this._context.globalStorageUri;
-        const previewUri = vscode.Uri.joinPath(storageUri, 'preview.png');
-
-        // convert local path of project files to a uri we can use in the webview
-        const previewSrc = panel.webview.asWebviewUri(previewUri);
-
-        // convert texture to png and write to disk so webview can read from disk
-        const previewData = await PVRLoader.readFile(document.uri);
-        await vscode.workspace.fs.createDirectory(storageUri);
-        await vscode.workspace.fs.writeFile(previewUri, previewData);
-
-        // use a content security policy to only allow loading styles from our extension directory
-        const mediaUri = vscode.Uri.joinPath(this._context.extensionUri, 'media');
-        const styleSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'viewer.css'));
-        const scriptSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'viewer.js'));
- 
-        // use a nonce to only allow a specific script to be run
-        const nonce = getNonce();
-
-        panel.webview.options = {
-            enableScripts: true,
-            localResourceRoots: [mediaUri, storageUri]
-        };
-
-        // setup initial content in new webview
-        panel.webview.html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src *; style-src ${panel.webview.cspSource}; script-src 'nonce-${nonce}';">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="${styleSrc}" rel="stylesheet">
-</head>
-<body>
-    <div class="main" data-vscode-context='{"webviewSection": "main", "preventDefaultContextMenuItems": true}'><img class="preview" draggable="false" src="${previewSrc}"></div>
-    <script nonce="${nonce}" src="${scriptSrc}"></script>
-</body>
-</html>`;
+        document.initPanel(panel, this._context);
     }
 }
 
